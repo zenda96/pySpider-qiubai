13a7033595c25a74b981556cbcca88795faaea97
zenda <1606726646@qq.com>
2018-03-30 11:05:52 +0800

    my.wisedu.com模拟登陆

--- myWiseduCom/myWiseduCom.py
+++ myWiseduCom/myWiseduCom.py
@@ -28,8 +28,9 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
 }
 data = {
-    'username':'01317009',
-    'password':'Zenda96!',
+    # 替换成自己的用户名和密码
+    # 'username':'',
+    # 'password':'',
     'lt':lt,
     'dllt':dllt,
     'execution':execution,
